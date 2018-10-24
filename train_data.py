424a94db91d2613a87f358b462c078913972b1a9
Himanshu Aggarwal <himanshua04@gmail.com>
2018-10-24 15:36:14 +0530

    to train on data

---
+++ train_data.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+from PIL import Image
+import cv2
+
+recog=cv2.face.LBPHFaceRecognizer_create();
+path='dataset'
+
+def getImagewithID(path):
+    imagePath=[os.path.join(path,f) for f in os.listdir(path)]
+    faces=[]
+    IDs=[]
+
+    for imgpath in imagePath:
+        faceImg=Image.open(imgpath)
+        faceNp=np.array(faceImg,'uint8')
+        ID=int(os.path.split(imgpath)[-1].split('_')[0])
+        faces.append(faceNp)
+        IDs.append(ID)
+        cv2.imshow('training',faceNp)
+        cv2.waitKey(10)
+
+    return IDs,faces
+
+IDs,faces=getImagewithID(path)
+recog.train(faces,np.array(IDs))
+recog.write('trainingData.yml')
+cv2.destroyAllWindows()
